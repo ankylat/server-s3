28af511fa38729f2d5ca22eef5f492b658a54fa1
jp <jp@jps-MacBook-Air.local>
2024-04-04 00:08:32 -0300

    aloja

--- app.js
+++ app.js
@@ -276,7 +276,7 @@ app.post("/start-session", checkIfValidUser, async (req, res) => {
         randomUUID: randomUUID,
         mentorIndex: ankyMentor.mentorIndex,
         walletAddress: userWallet,
-        ankyverseDay: 3,
+        ankyverseDay: 5,
       },
     });
 
