40031b6e92917a7ec5a5c9f03a694e129a83ec14
jp <jp@jps-MacBook-Air.local>
2024-04-11 18:42:52 -0400

    aloja

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,74 +1,68 @@
+const express = require("express");
 const fs = require("fs").promises;
 const path = require("path");
-const express = require("express");
 const router = express.Router();
 
 router.get("/:wink", async (req, res) => {
-  // const apiKey = req.headers["x-api-key"]; // typically API keys are sent in headers
-  // const expectedApiKey = process.env.BRUNO_API_KEY; // This should be the actual value of your API key
+  const apiKey = req.headers["x-api-key"];
+  const expectedApiKey = process.env.BRUNO_API_KEY;
 
-  // if (!apiKey || apiKey !== expectedApiKey) {
-  //   // If the API key is not present or doesn't match, return an unauthorized error
-  //   return res.status(401).json({ error: "Unauthorized access" });
-  // }
+  if (!apiKey || apiKey !== expectedApiKey) {
+    return res.status(401).json({ error: "Unauthorized access" });
+  }
 
   const wink = req.params.wink;
-  try {
-    const bookDir = path.join(__dirname, "../lib/book"); // Adjust the path as necessary
-    const chapter = await fs.readFile(
-      path.join(bookDir, `chapters/${wink - 1}.txt`),
-      "utf8"
-    );
-    const promptEnglish = await fs.readFile(
-      path.join(bookDir, `daily-prompts/en/${wink}.txt`),
-      "utf8"
-    );
-    const promptSpanish = await fs.readFile(
-      path.join(bookDir, `daily-prompts/es/${wink}.txt`),
-      "utf8"
-    );
-    const prompts = { en: promptEnglish, es: promptSpanish };
-    const userWritings = await fs.readFile(
-      path.join(bookDir, `daily-writings/${wink}.txt`),
-      "utf8"
-    );
-    const userFeedbackForChapter = await fs.readFile(
-      path.join(bookDir, `feedback-from-users/${wink - 1}.txt`),
-      "utf8"
-    );
-    const summaryOfChapter = await fs.readFile(
-      path.join(bookDir, `summary-of-chapters/${wink - 1}.txt`),
-      "utf8"
-    );
-    const superPrompt = await fs.readFile(
-      path.join(bookDir, `super-prompts/${wink - 1}.txt`),
-      "utf8"
-    );
-    const jpComments = await fs.readFile(
-      path.join(bookDir, `jp-comments/${wink}.txt`),
-      "utf8"
-    );
+  const bookDir = path.join(__dirname, "../lib/book");
 
-    const dataToReturn = {
-      wink,
-      prompts,
-      userWritings,
-      superPrompt,
-      chapter,
-      summaryOfChapter,
-      userFeedbackForChapter,
-      jpComments,
-    };
-    res.json(dataToReturn);
-  } catch (error) {
-    console.log(
-      `There was an error retrieving the book data for wink ${wink}`,
-      error
-    );
-    return res.status(500).json({
-      message: `There was an error retrieving the book data for wink ${wink}`,
-    });
+  // Helper function to read file and return null if doesn't exist or is empty
+  async function safeReadFile(filePath) {
+    try {
+      const content = await fs.readFile(filePath, "utf8");
+      return content || null;
+    } catch (error) {
+      return null; // Return null if the file doesn't exist or any error occurs
+    }
   }
+
+  // Using the helper function to safely read the files
+  const chapter = await safeReadFile(
+    path.join(bookDir, `chapters/${wink - 1}.txt`)
+  );
+  const promptEnglish = await safeReadFile(
+    path.join(bookDir, `daily-prompts/en/${wink}.txt`)
+  );
+  const promptSpanish = await safeReadFile(
+    path.join(bookDir, `daily-prompts/es/${wink}.txt`)
+  );
+  const prompts = { en: promptEnglish, es: promptSpanish };
+  const userWritings = await safeReadFile(
+    path.join(bookDir, `daily-writings/${wink}.txt`)
+  );
+  const userFeedbackForChapter = await safeReadFile(
+    path.join(bookDir, `feedback-from-users/${wink - 1}.txt`)
+  );
+  const summaryOfChapter = await safeReadFile(
+    path.join(bookDir, `summary-of-chapters/${wink - 1}.txt`)
+  );
+  const superPrompt = await safeReadFile(
+    path.join(bookDir, `super-prompts/${wink - 1}.txt`)
+  );
+  const jpComments = await safeReadFile(
+    path.join(bookDir, `jp-comments/${wink}.txt`)
+  );
+
+  const dataToReturn = {
+    wink,
+    prompts,
+    userWritings,
+    superPrompt,
+    chapter,
+    summaryOfChapter,
+    userFeedbackForChapter,
+    jpComments,
+  };
+
+  res.json(dataToReturn);
 });
 
 module.exports = router;
